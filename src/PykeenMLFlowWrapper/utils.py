ee2b9571177b4cd350e1255a0cd9e82b4f75346d
Rodrigo-A-Pereira <rarpereira20@gmail.com>
2022-06-06 16:04:52 +0100

    add param and metric log

--- src/PykeenMLFlowWrapper/utils.py
+++ src/PykeenMLFlowWrapper/utils.py
@@ -5,18 +5,23 @@ from sys import version_info
 import torch
 import pykeen
 
-def save_model(pipeline_result, model_name):
+def save_model(pipeline_result, model_name, param_tracker=None):
     "Save Pykeen model and register it on MLFlow"
 
 
     save_path=f"{model_name}_saved"
     pipeline_result.save_to_directory(save_path)
+
+
+    
     
     model_path = os.path.join(save_path, "trained_model.pkl")
     triples_factory_path = os.path.join(save_path, "training_triples")
 
     artifacts = {"pykeen_triples_path": triples_factory_path,
                  "pykeen_model_path": model_path}
+
+    print(artifacts)
 
     
     PYTHON_VERSION = f"{version_info.major}.{version_info.minor}.{version_info.micro}"
@@ -37,6 +42,12 @@ def save_model(pipeline_result, model_name):
     }
 
     with mlflow.start_run() as run:
+
+        if param_tracker is not None:
+            mlflow.log_params(pykeen.utils.flatten_dictionary(param_tracker.configuration))
+
+        mlflow.log_metrics(pipeline_result.metric_results.to_flat_dict())
+
         return mlflow.pyfunc.log_model(
             artifact_path=model_name,
             python_model=PykeenWrapper(),
